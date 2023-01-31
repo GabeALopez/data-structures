3f2dcec39559e0bb274264f79c4384f087f55175
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2023-01-31 12:45:19 -0500

    Finished Project

--- Lopez_Gabriel_Linked_List.cpp
+++ Lopez_Gabriel_Linked_List.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//class that holds a node
 class Node
 {
 
@@ -13,54 +14,21 @@ class Node
 };
 
 
+//main linked list class
 class linkedList
 {
 
-    public:
-        int length;
-        Node* head;
-
-        linkedList()
-        {
-
-            length = 0;
-            head = NULL;
-
-        }        
-
-        void pushFront(int pKey)
-        {
-
-            addAtPosition(0,pKey);
-
-        }
-
-        Node* topFront()
-        {
-
-            if(head->next == NULL)
-            {
-                return NULL; 
-            }           
-
-            return head->next;
-
-        }
-
-        int popFront()
-        {
-
-            deleteAtPosition(0);
-
-        }
-
+    private:
+        //adds a node at a partciular point
         void addAtPosition(int position, int n)        
         {
  
+            //nodes that are need to iterate through list
             Node* prevNode = new Node();
             Node* currentNode = new Node();
             Node* temp = new Node();
 
+            //put node if it is at position 0
             if(position == 0)
             {
 
@@ -69,6 +37,7 @@ class linkedList
                 this->head = temp;
 
             }
+            //put node at n position
             else
             {
 
@@ -77,15 +46,6 @@ class linkedList
                 {
 
                     prevNode = currentNode;
-
-                    if(currentNode->next == NULL)
-                    {
-
-                        cout << "Out of Bounds" << endl;
-                        return;
-
-                    }
-
                     currentNode = currentNode->next;
 
 
@@ -96,13 +56,16 @@ class linkedList
 
             }
 
+            //increase the length
             this->length++;
 
         }
 
+        //same as addAtPosition but for deleting
         void deleteAtPosition(int position)
         {
 
+            
             Node* preNode = new Node();
             Node* currentNode = new Node();
             currentNode = this->head;
@@ -122,15 +85,6 @@ class linkedList
                 {
 
                     preNode = currentNode;
-
-                    if(currentNode->next == NULL)
-                    {
-
-                        cout << "Out of Bounds" << endl;
-                        return;
-
-                    } 
-
                     currentNode = currentNode->next;
 
 
@@ -143,31 +97,78 @@ class linkedList
             this->length--;
 
         }
+    
+    public:
+        //hold the length and create the head of the linked list
+        int length;
+        Node* head;
 
+        //creates an initial linked list
+        linkedList()
+        {
 
-        void pushBackKey(int pKey)
+            length = 0;
+            head = NULL;
+
+        }        
+
+        //create a new node at the beginning
+        void pushFront(int pKey)
+        {
+
+            addAtPosition(0,pKey);
+
+        }
+
+        //return the beginning node
+        void topFront()
+        {
+
+            if(head->next == NULL)
+            {
+                cout << "NULL" << endl; 
+            }           
+
+            cout << head->key << endl;;
+
+        }
+
+        //delete the beginning node 
+        int popFront()
+        {
+
+            deleteAtPosition(0);
+
+        }
+
+        
+
+        //add a node at the end of the list
+        void pushBack(int pKey)
         {
 
             addAtPosition(length, pKey);
 
         }
 
-        Node* topBack()
+        //return the node at the end of the list
+        void topBack()
         {
 
             Node* newNode = this->head;
-            for(int i = 0; i < length; i++)
+            for(int i = 0; i < length-1; i++)
             {
 
                 newNode = newNode->next;
 
             }
 
-            return newNode;
+            cout << newNode->key << endl;
 
 
         }
 
+        //delete the node at the end of the list
         void popBack()
         {
 
@@ -175,6 +176,7 @@ class linkedList
 
         }
 
+        //finds if key is within list
         bool findKey(int pKey)
         {
 
@@ -198,11 +200,220 @@ class linkedList
 
         }
 
+        //checks if list is empty
+        bool empty()
+        {
+
+            if(head == NULL)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+        //adds node before a specified position
+        void addBefore(int pNode, int pKey)
+        {
+
+            addAtPosition(pNode, pKey);
+
+        }
+
+        //adds node after a specified position
+        void addAfter(int pNode, int pKey)
+        {
+
+           addAtPosition(pNode+1, pKey); 
+
+        }
+
+
+
+        //returns size of list
+        int size()
+        {
+
+            return length;
+
+        }
+
+        //displays all values in list
+        void displayAll()
+        {
+
+            Node *newNode = this->head;
+
+            int i = 1;
+
+            while(newNode)
+            {
+
+                cout << newNode->key << endl;
+                newNode = newNode->next;
+                i++;
+
+            }
+
+
+        }
+
+        //replaces a key with a new key
+        void replaceKey(int pNode, int pKey)
+        {
+
+            Node* prevNode = new Node();
+            Node* currentNode = new Node();
+
+            if(pNode == 0)
+            {
+
+                this->head->key = pKey;                 
+
+            }
+            else
+            {
+
+                currentNode = this->head;
+                for(int i = 0; i < pNode; i++)
+                {
+
+                    prevNode = currentNode;
+                    currentNode = currentNode->next; 
+
+
+                }
+
+                prevNode->key = pKey;
+            }
+
+        }
+
+
+
 };
 
 int main()
 {
 
+    //implementation
+    linkedList list;    
+
+    //push values into list
+    list.pushFront(25); 
+    list.pushFront(50); 
+    list.pushFront(90); 
+    list.pushFront(40); 
+    list.pushBack(35); 
+
+    list.displayAll();
+    cout << endl;
+
+    list.topFront();
+
+    cout << endl;
+
+    list.topBack();
+
+    cout << endl;
+
+    list.popFront();
+    list.displayAll();
+
+    cout << endl;
+
+    list.popBack();
+    list.displayAll();
+
+    cout << endl;
+
+    //replacing values
+    list.pushBack(10);
+    list.pushBack(12);
+    list.pushBack(14);
+    list.displayAll();
+
+    cout << endl;
+
+    //running find key checks
+    if(list.findKey(25))
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+
+    if(list.findKey(6))
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+
+    //check if empty
+    if(list.empty())
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+
+    cout << endl;
+
+    //adding nodes, returning size, and replacing key
+    list.addBefore(3,94);
+
+    list.displayAll();
+
+    cout << endl;
+
+    list.addAfter(2,5);
+    list.displayAll();
+
+    cout << endl;
+
+    list.size();
+
+    cout << endl;
+
+    list.replaceKey(6,87);
+
+    list.displayAll();
+
+    cout << endl;
+
+    //poping them all values off
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    list.popFront();
+
+
+    if(list.empty())
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
 
 
 }
